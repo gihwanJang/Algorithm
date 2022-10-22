9456fcd082d296dad36719fb3b42f91334bd0c60
gihwanJang <gihwan319@gmail.com>
2022-10-22 22:46:59 +0900

    Baekjoon11382

---
+++ cpp/Baekjoon11382.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[]){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    long long a, b, c;
+    cin >> a >> b >> c;
+
+    cout << a + b + c << "\n";
+    return 0;
+}
