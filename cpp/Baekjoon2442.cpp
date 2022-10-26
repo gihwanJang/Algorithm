e29a213f36c41d55b6f42ec73176594c99cdd34c
장기환 <jang-gihwan@jang-gihwan-ui-MacBookPro.local>
2022-10-26 20:00:22 +0900

    Baekjoon2442

---
+++ cpp/Baekjoon2442.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[]){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N;
+    cin >> N;
+    
+    for(int i = 1; i <= N; ++i){
+        for(int j = 1; j <= N-i; ++j) 
+            cout << " ";
+        for (int j = 1; j <= 2*i-1; ++j)
+            cout << "*";
+        cout << "\n";
+    }
+    return 0;
+}
