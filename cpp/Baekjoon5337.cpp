186d19075ef08adf95a2e0e302b2b9c3546bba2b
gihwanJang <gihwan319@gmail.com>
2022-11-16 12:02:27 +0900

    Baekjoon5337

---
+++ cpp/Baekjoon5337.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[]){
+    ios_base::sync_with_stdio(false);
+    cout.tie(NULL);
+
+    cout << ".  .   ." << "\n";
+    cout << "|  | _ | _. _ ._ _  _" << "\n";
+    cout << "|/\\|(/.|(_.(_)[ | )(/." << "\n";
+
+    return 0;
+}
