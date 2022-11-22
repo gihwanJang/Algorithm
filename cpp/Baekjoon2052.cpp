27816a83cddba5765fe6422bf9e01572a8f645b0
장기환 <jang-gihwan@jang-gihwan-ui-MacBookPro.local>
2022-11-23 08:16:04 +0900

    Baekjoon2052

---
+++ cpp/Baekjoon2052.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+int main(int argc, char const *argv[]){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int N;
+    cin >> N;
+
+    double answer = pow(0.5, N);
+    
+    printf("%.*f",N,answer);
+    return 0;
+}
