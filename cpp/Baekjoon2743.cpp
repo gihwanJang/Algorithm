a6f6446bfa5e09c24fff76766097bfa55bcd94bf
장기환 <jang-gihwan@jang-gihwan-ui-MacBookPro.local>
2022-12-14 23:40:27 +0900

    Baekjoon2743

---
+++ cpp/Baekjoon2743.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[]){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    string s;
+    cin >> s;
+
+    cout << s.length() << "\n";
+    return 0;
+}
