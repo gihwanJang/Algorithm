bb78a8eb20a3339ee740f99b9ebbc116daeb6430
gihwanJang <gihwan319@gmail.com>
2022-12-14 23:40:27 +0900

    Baekjoon2743

---
+++ cpp/Baekjoon1254.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int makePalindrop(string s){
+    
+}
+
+int main(int argc, char const *argv[]){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    string s;
+    cin >> s;
+
+    cout << makePalindrop(s) << "\n";
+    return 0;
+}
