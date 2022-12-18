bd15cb4e326ff5fcf0996391b6052cc4cb43b946
장기환 <jang-gihwan@jang-gihwan-ui-MacBookPro.local>
2022-12-18 19:31:17 +0900

    Baekjoon1254

--- cpp/Baekjoon1254.cpp
+++ cpp/Baekjoon1254.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
-int makePalindrop(string s){
-    
+bool isPalindrome(string str){
+    int len = str.length(), half = (len >> 1);
+    for(int i = 0; i <= half; ++i)
+        if(str[i] != str[len - i - 1])
+            return false;
+    return true;
+}
+
+int makePalindrop(string str){
+    int len = str.length();
+    for(int i = 0; i < len; ++i)
+        if(isPalindrome(str.substr(i)))
+            return i + len;
+    return (len << 1);
 }
 
 int main(int argc, char const *argv[]){
