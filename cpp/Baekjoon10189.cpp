00218cf94312ffe5bfebaaa546fdb3343a95e252
장기환 <jang-gihwan@jang-gihwan-ui-MacBookPro.local>
2022-12-19 20:43:52 +0900

    Baekjoon10189

---
+++ cpp/Baekjoon10189.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[]){
+    ios_base::sync_with_stdio(false);
+    cout.tie(NULL);
+
+    cout << "#  # #### #### #  #" << "\n";
+    cout << "#### #  # #  # # #" << "\n";
+    cout << "#### #  # #  # # #" << "\n";
+    cout << "#  # #### #### #  #" << "\n";
+    return 0;
+}
