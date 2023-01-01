7e2df1616725ef42d2bdfbad70570a9576625a20
장기환 <jang-gihwan@jang-gihwan-ui-MacBookPro.local>
2023-01-01 16:33:55 +0900

    Baekjoon2845

---
+++ cpp/Baekjoon2393.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[]){
+    ios_base::sync_with_stdio(false);
+    cout.tie(NULL);
+
+    cout << "  ___  ___  ___" << "\n";
+    cout << "  | |__| |__| |" << "\n";
+    cout << "  |           |" << "\n";
+    cout << "   \\_________/" << "\n";
+    cout << "    \\_______/" << "\n";
+    cout << "     |     |" << "\n";
+    cout << "     |     |" << "\n";
+    cout << "     |     |" << "\n";
+    cout << "     |     |" << "\n";
+    cout << "     |_____|" << "\n";
+    cout << "  __/       \\__" << "\n";
+    cout << " /             \\" << "\n";
+    cout << "/_______________\\" << "\n";
+
+    return 0;
+}
