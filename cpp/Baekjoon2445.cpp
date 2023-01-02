5c60dab89cda00e1cce3b1922bcf9df92e2a2da0
gihwanJang <gihwan319@gmail.com>
2023-01-02 13:12:12 +0900

    Baekjoon2445

---
+++ cpp/Baekjoon2445.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[]){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N;
+    cin >> N;
+
+    for(int i = 1; i <= N; ++i){
+        for(int j = 0; j < i; ++j)
+            cout << "*";
+        for(int j = i; j < N; ++j)
+            cout << " ";
+        for(int j = i; j < N; ++j)
+            cout << " ";
+        for(int j = 0; j < i; ++j)
+            cout << "*";
+        cout << "\n";
+    }
+    for(int i = N - 1; i > 0 ; --i){
+        for(int j = 0; j < i; ++j)
+            cout << "*";
+        for(int j = i; j < N; ++j)
+            cout << " ";
+        for(int j = i; j < N; ++j)
+            cout << " ";
+        for(int j = 0; j < i; ++j)
+            cout << "*";
+        cout << "\n";
+    }
+    return 0;
+}
