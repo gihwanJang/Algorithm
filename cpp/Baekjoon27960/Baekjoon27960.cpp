b18d1da3073bef7bc4657ff5160704febf13ba73
gihwanJang <gihwan319@gmail.com>
2023-04-18 19:30:30 +0900

    Baekjoon27960

---
+++ cpp/Baekjoon27960/Baekjoon27960.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[]){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int a, b;
+    cin >> a >> b;
+
+    cout << (a^b) << "\n";
+    return 0;
+}
