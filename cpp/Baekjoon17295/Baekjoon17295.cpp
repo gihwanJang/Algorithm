8631133471b7964f80a4bbc5f2e2011107122ab6
gihwanJang <gihwan319@gmail.com>
2023-05-14 19:19:45 +0900

    Baekjoon17295

---
+++ cpp/Baekjoon17295/Baekjoon17295.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[]){
+    ios_base::sync_with_stdio(false);
+    cout.tie(NULL);
+    cout.tie(NULL);
+
+    string s;
+    cin >> s;
+
+    cout << "Avengers: Endgame" << "\n";
+    return 0;
+}
