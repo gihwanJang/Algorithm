b6ee9d2ce59b5424174ad5eb77916006de1a528d
gihwanJang <gihwan319@gmail.com>
2023-07-04 20:01:34 +0900

    Baekjoon1402

---
+++ cpp/Baekjoon1402/Baekjoon1402.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int T;
+    cin >> T;
+
+    while(T--)
+    {
+        int a, b;
+        cin >> a >> b;
+
+        cout << "yes\n" << "\n";
+    }
+    return 0;
+}
