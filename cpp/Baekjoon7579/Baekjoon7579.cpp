5ea1096b02c166fb561faa4f13e3a980d5c0afbe
gihwanJang <gihwan319@gmail.com>
2023-08-20 22:08:22 +0900

    Baekjoon7579

--- cpp/Baekjoon7579/Baekjoon7579.cpp
+++ cpp/Baekjoon7579/Baekjoon7579.cpp
@@ -1,15 +1,49 @@
-#include <cstdio>
-#include <utility>
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
 using namespace std;
 
+struct Item{ int weight, value; };
+
+int getProfitSchedule(vector<Item>&items, int n, int m, int sum)
+{
+    int profit = 0;
+    vector<vector<int>> dp(n+1, vector<int>(sum+1));
+
+    for(int i = 1; i <= n; ++i)
+        for(int j = 0; j <= sum; ++j)
+        {
+            if(j - items[i].weight >= 0)
+                dp[i][j] = max(dp[i-1][j], dp[i - 1][j - items[i].weight] + items[i].value);
+            else
+                dp[i][j] = dp[i-1][j];
+        }
+
+    for(int i = 0; i <= sum; ++i)
+        if(dp[n][i] >= m)
+            return i;
+
+    return 0;
+}
+
 int main(int argc, char const *argv[]){
-    int n, m, ans = 10000000;
-    pair<int, int> dp[101][101];
-    
-    scanf("%d %d", &n, &m);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n, m, sum = 0;
+    cin >> n >> m;
+
+    vector<Item> items(n+1);
     for(int i = 1; i <= n; ++i)
-        scanf("%d", &dp[0][i].first);
+        cin >> items[i].value;
     for(int i = 1; i <= n; ++i)
-        scanf("%d", &dp[0][i].second);
+    {
+        cin >> items[i].weight;
+        sum += items[i].weight;
+    }
+
+    cout << getProfitSchedule(items, n, m, sum) << "\n";
     return 0;
 }
