9c546cd54ce10cdd794f00174a48cf8a3843e89d
gihwanJang <gihwan319@gmail.com>
2023-12-21 10:07:11 +0900

    Baekjoon5747

--- java/Baekjoon5747/Baekjoon5747.java
+++ java/Baekjoon5747/Baekjoon5747.java
@@ -1,52 +1,28 @@
 import java.io.*;
-import java.util.*;
-
-class Problem {
-    int[] mary, john;
-
-    public Problem() {
-        mary = new int[2];
-        john = new int[2];
-    }
-
-    public int solve() {
-        makeOdd(0, 1);
-        makeOdd(1, 0);
-        return Math.max(mary[0], mary[1]);
-    }
-
-    private void makeOdd(int m, int j) {
-        if(mary[m] > john[j]) {
-            mary[m] -= john[j];
-            john[j] = 0;
-        } else {
-            john[j] -= mary[m];
-            mary[m] = 0;
-        }
-    }
-}
 
 public class Baekjoon5747 {
     public static void main(String[] args) throws Exception {
-        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+        String s;
+        int[] num;
         StringBuilder sb = new StringBuilder();
+        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
 
-        while(true) {
-            int n = Integer.parseInt(br.readLine());
-            if(n == 0) {
-                break;
+        while(n != 0) {
+            num = new int[2];
+
+            s = br.readLine();
+            for(int i = 0; i < n; ++i) {
+                ++num[(s.charAt(i*2) - '0')%2];
             }
 
-            Problem problem = new Problem();
-            StringTokenizer st = new StringTokenizer(br.readLine());
+            s = br.readLine();
             for(int i = 0; i < n; ++i) {
-                ++problem.mary[Integer.parseInt(st.nextToken())%2];
+                --num[1 - (s.charAt(i*2) - '0')%2];
             }
-            st = new StringTokenizer(br.readLine());
-            for(int i = 0; i < n; ++i) {
-                ++problem.john[Integer.parseInt(st.nextToken())%2];
-            }
-            sb.append(problem.solve()).append("\n");
+
+            sb.append(Math.max(Math.abs(num[0]), Math.abs(num[1]))).append("\n");
+            n = Integer.parseInt(br.readLine());
         }
 
         System.out.print(sb.toString());
