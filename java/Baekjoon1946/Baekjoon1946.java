a02e83c11f6815acc39556efe8557bd0acc8d969
gihwanJang <gihwan319@gmail.com>
2024-01-01 15:07:08 +0900

    Baekjoon1946

--- java/Baekjoon1946/Baekjoon1946.java
+++ java/Baekjoon1946/Baekjoon1946.java
@@ -1,46 +1,56 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
+
+class Rank implements Comparable<Rank> {
+    int f, s;
+    public Rank(int f, int s) {
+        this.f = f;
+        this.s = s;
+    }
+    @Override
+    public int compareTo(Rank o) {
+        return f - o.f;
+    }
+}
+
+class Problem {
+    int n;
+    Rank[] ranks;
+    public Problem(int n) {
+        this.n = n;
+        ranks = new Rank[n];
+    }
+    public int solve() {
+        Arrays.sort(ranks);
+        int cnt = 1, min = ranks[0].s;
+        for(int i = 1; i < n; ++i) {
+            if(ranks[i].s < min) {
+                ++cnt;
+            }
+            min = Math.min(min, ranks[i].s);
+        }
+        return cnt;
+    }
+}
 
 public class Baekjoon1946 {
-    public static class Applicant implements Comparable<Applicant>{
-        int socre, rank;
-        public Applicant(int socre, int rank){
-            this.socre = socre;
-            this.rank = rank;
-        }
-        @Override
-        public int compareTo(Applicant t){
-            return this.rank-t.rank;
-        }
-    }
-    public static int solution(int N, Applicant[] applicants){
-        int ans = 0;
-        int min = applicants[0].socre;
-        Arrays.sort(applicants);
-        for (int i = 1; i<N; ++i){
-            if (applicants[i].socre < min) {
-                ++ans;
-                min = applicants[i].socre;
-            }
-        }
-        return ans;
-    }
     public static void main(String[] args) throws NumberFormatException, IOException {
+        StringBuilder sb = new StringBuilder();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         for(int T=Integer.parseInt(br.readLine()); T>0; --T){
-            int N = Integer.parseInt(br.readLine());
-            Applicant[] applicants = new Applicant[N];
-            for(int i=0; i<N; ++i){
+            Problem problem = new Problem(Integer.parseInt(br.readLine()));
+
+            for(int i = 0; i < problem.n; ++i){
                 StringTokenizer st = new StringTokenizer(br.readLine());
                 int socre = Integer.parseInt(st.nextToken());
                 int rank = Integer.parseInt(st.nextToken());
-                applicants[i] = new Applicant(socre, rank);
+                problem.ranks[i] = new Rank(socre, rank);
             }
 
-            System.out.println(solution(N, applicants));
+            sb.append(problem.solve()).append("\n");
         }
+
+        System.out.print(sb.toString());
     }
 }
