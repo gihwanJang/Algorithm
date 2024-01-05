948577e1cf0aecc270cf102dfd5a28d7bb5c5575
gihwanJang <gihwan319@gmail.com>
2024-01-05 14:35:59 +0900

    Baekjoon8871

---
+++ java/Baekjoon8871/Baekjoon8871.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+public class Baekjoon8871 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine())+1;
+        System.out.println(n*2 + " " + n*3);
+    }    
+}
