ab84d2d60f96c0d536e7009f9f9054f4f0a5935f
gihwanJang <90659997+gihwanJang@users.noreply.github.com>
2024-09-20 21:01:07 +0900

    Baekjoon25215

---
+++ cpp/Baekjoon25215/Baekjoon25215.java
@@ -0,0 +1,67 @@
+import java.io.*;
+import java.util.*;
+
+class Problem {
+    boolean startIsLowwer;
+    List<Integer> charList;
+
+    public Problem() {
+        try {
+            input();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        charList = new ArrayList<>(s.length());
+        charList.add(1);
+        startIsLowwer = isLowwer(s.charAt(0));
+        for (int i = 1; i < s.length(); ++i) {
+            if (isLowwer(s.charAt(i-1)) == isLowwer(s.charAt(i))) {
+                charList.set(charList.size()-1, charList.get(charList.size()-1) + 1);
+            } else {
+                charList.add(1);
+            }
+        }
+        solve();
+    }
+
+    private boolean isLowwer(char c) {
+        return ('a' <= c && c <= 'z');
+    }
+
+    private void solve() {
+        int count = 0;
+        boolean dia = false;
+        boolean lowwer = startIsLowwer;
+
+        for (int i = 0; i < charList.size(); ++i) {
+            if (charList.get(i) == 1) {
+                if ((lowwer && !dia) || (!lowwer && dia)) { // 대/소 문자가 일치
+                    count += 1;
+                } else {
+                    count += 2;
+                }
+            } else {
+                count += charList.get(i);
+                if (!((lowwer && !dia) || (!lowwer && dia))) { // 대/소 문자가 불일치
+                    count += 1;
+                    dia = !dia;
+                }
+            }
+            lowwer = !lowwer;
+            // System.out.println(count);
+        }
+
+        System.out.println(count);
+    }
+}
+
+public class Baekjoon25215 {
+    public static void main(String[] args) {
+        new Problem();
+    }
+}
